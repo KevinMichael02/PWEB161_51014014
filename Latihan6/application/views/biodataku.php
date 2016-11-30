08a970b19b8aaeb8b2a4f882722ec3321b0f916f
KevinMichael02 <kevin_michael02@yahoo.com>
2016-11-30 23:31:34 +0800

    Latihan 6
    xampp

---
+++ Latihan6/application/views/biodataku.php
@@ -0,0 +1,18 @@
+
+<center><h1>Biodata</h1></center>
+<hr>
+
+<center>
+<h2>
+					Nama depan: Kevin<br>
+					Nama belakang : Michael<br>
+					Nim : 51014014<br>
+					Tempat, Tanggal lahir : 02-Agustus-1996<br>
+					Umur : 20 Tahun<br>
+					Jenis Kelamin : Laki-Laki<br>
+					Agama : Khatolik<br>
+					Tinggi : 174 cm<br>
+					E-mail : Kevin_michael@yahoo.com<br>
+					No. Telp : 082191789853<br>
+<h2>
+</center>
---
+++ Latihan6/application/views/pengalaman.php
@@ -0,0 +1,11 @@
+
+<center><h1>Pengalaman Tugas</h1></center>
+<hr>
+
+<center>
+<h2>
+ <br>Pengalaman selama membuat tugas menjadikan saya pribadi yang lebih disiplin karena berpatokan dengan waktu <br>
+Namun kadang persyaratan mendapatkan poin kurang jelas jadi saya tidak tau mau melakukan apa supaya dapat poin lebih <br>
+<br>tapi saya belajar untuk lebih efisien dalam penggunaan coding<br>
+
+</center>
